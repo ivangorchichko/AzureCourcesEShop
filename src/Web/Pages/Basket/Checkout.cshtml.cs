f6b5be2cd36fecc501cf59a20c24b313ab5a5615
Ivan_Harchychko <Ivan_Harchychko@epam.com>
2023-05-11 19:05:47 +0300

    Complete  Service Bus. Event Hub. Event Grid

--- src/Web/Pages/Basket/Checkout.cshtml.cs
+++ src/Web/Pages/Basket/Checkout.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using Ardalis.GuardClauses;
+using Azure.Messaging.ServiceBus;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -57,23 +58,31 @@ public class CheckoutModel : PageModel
 
             var updateModel = items.ToDictionary(b => b.Id.ToString(), b => b.Quantity);
             await _basketService.SetQuantities(BasketModel.Id, updateModel);
-            var order = await _orderService.CreateOrderAsync(BasketModel.Id, new Address("123 Main St.", "Kent", "OH", "United States", "44240"));
+            await _orderService.CreateOrderAsync(BasketModel.Id, new Address("123 Main St.", "Kent", "OH", "United States", "44240"));
             await _basketService.DeleteBasketAsync(BasketModel.Id);
-            var orderDetails = new
-            {
-                ShippingAddress = order.ShipToAddress,
-                Items = order.OrderItems,
-                Price = order.Total()
-            };
-           
-            var client = new HttpClient();
-            var jsonDictionary = JsonConvert.SerializeObject(orderDetails);
+            var jsonDictionary = JsonConvert.SerializeObject(updateModel);
             var content = new StringContent(jsonDictionary, Encoding.UTF8, "application/json");
-            await client.PostAsync(
-                "http://localhost:7071/api/OrderItemsReserver",
-             //   "https://ivangorchichkofuncapp.azurewebsites.net/api/OrderItemsReserver?code=641XBQTuFvKl8COFj_ZwLyLKtNdQzL-VT51l0HK5zhWMAzFuJ9YhCQ==",
-                content
-            );  
+            await using var client = new ServiceBusClient(
+                "Endpoint=sb://gorchichkosb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=gFzLI2U+aVpKMC5pBe8kH4uCF2EZg6bbp+ASbNpBgBM="
+                );
+            await using var sender = client.CreateSender("orders");
+            var message = new ServiceBusMessage(jsonDictionary);
+            await sender.SendMessageAsync(message);
+            //var orderDetails = new
+            //{
+            //    ShippingAddress = order.ShipToAddress,
+            //    Items = order.OrderItems,
+            //    Price = order.Total()
+            //};
+
+            //var client = new HttpClient();
+            //var jsonDictionary = JsonConvert.SerializeObject(orderDetails);
+            //var content = new StringContent(jsonDictionary, Encoding.UTF8, "application/json");
+            //await client.PostAsync(
+            //    "http://localhost:7071/api/OrderItemsReserver",
+            // //   "https://ivangorchichkofuncapp.azurewebsites.net/api/OrderItemsReserver?code=641XBQTuFvKl8COFj_ZwLyLKtNdQzL-VT51l0HK5zhWMAzFuJ9YhCQ==",
+            //    content
+            //);  
         }
         catch (EmptyBasketOnCheckoutException emptyBasketOnCheckoutException)
         {
